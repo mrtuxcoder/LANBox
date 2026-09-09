945528e30b55a5c061bbaf2447142e6e0192573e
mrtuxcoder <guganrajrengaraju@gmail.com>
2026-09-09 10:10:55 +0530

    add dockerfile, docker-compose and few edits

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
-export const API_URL =
-  `${window.location.protocol}//` +
-  `${window.location.hostname}:3000/api`;
+// export const API_URL =
+//   `${window.location.protocol}//` +
+//   `${window.location.hostname}:3000/api`;
+
+// The browser will automatically attach the correct protocol, IP, and port of whoever is visiting
+export const API_URL = '/api'; 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // (or vue, etc)
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,      
+    port: 5173       
+  }
 })
